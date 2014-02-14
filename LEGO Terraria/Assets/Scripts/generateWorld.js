d230605554e01f7c2dcf4d258d94d1eba73a4e7a
seaside98 <add98@icloud.com>
2014-02-13 23:10:53 -0600

    Basic demo
    Arrows to move, space to jump

---
+++ LEGO Terraria/Assets/Scripts/generateWorld.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var maxHeight : int = 8;
+var dirt_block : Transform;
+var parentObject : Transform;
+
+function Start () {
+	//This will eventually be converted a random world generator based off of a seed.
+	//Sizes are static for this demo
+	/*var blockArray = new int[16];
+	var maxHeight : int = 8;
+	
+	for (var a : int = 0; a < 16; a++) {
+		blockArray[a] = new int[32];
+	}
+	
+	for (var b : int in blockArray) {
+		for (var bb : int in blockArray[b]) {
+			if (bb <= maxHeight) {
+				blockArray[b][bb] = 1;
+			} else {
+				blockArray[b][bb] = 0;
+			}
+		}
+	}
+	
+	for (var c : int in blockArray) {
+		for (var cc : int in blockArray[c]) {
+			if (blockArray[c][cc] == 1) {
+				Instantiate(dirt_block, Vector2 (c-8, cc), Quaternion.identity);
+			}
+		}
+	}*/
+	for (var a : int = 0; a < 16; a++) {
+		for (var aa : int = 0; aa < 32; aa++) {
+			if (aa <= maxHeight) {
+				var object = Instantiate(dirt_block, Vector3 (a-8, aa, 4), Quaternion.identity) as Transform;
+				object.transform.parent = parentObject;
+			}
+		}
+	}
+}
+
+function Update () {
+
+}
---
+++ LEGO Terraria/Assets/Scripts/moveCharacter.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var speed : float = 6.0;
+var jumpSpeed : float = 50.0;
+
+private var moveDirection : Vector3 = Vector3.zero;
+private var grounded;
+
+function Update() {
+	moveDirection = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	moveDirection = transform.TransformDirection(moveDirection);
+	moveDirection *= speed;
+	
+	if (Input.GetButton ("Jump") && grounded) {
+		grounded = false;
+		moveDirection.y = jumpSpeed;
+	}
+	// Apply gravity
+	//moveDirection.y -= gravity * Time.deltaTime;
+	
+	// Move the controller
+	transform.Translate(moveDirection * Time.deltaTime);
+}
+
+function OnCollisionEnter2D(col : Collision2D) {
+	grounded = true;
+}
