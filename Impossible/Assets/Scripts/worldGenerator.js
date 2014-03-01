c790a46fbba4c42d661838476673f985e21841ce
seaside98 <add98@icloud.com>
2014-02-28 23:22:15 -0600

    Creating Impossible project

---
+++ Impossible/Assets/Scripts/worldGenerator.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+static var blockValue : int[,,];
+
+var seed : int = 0;
+var octaves : int = 8;
+var frq : float = 50.0;
+var amp : float = 1.0;
+
+private var perlin : PerlinNoise;
+
+function Awake () {
+	perlin = new PerlinNoise(seed);
+	blockValue = new int[216, 216, 216];
+	
+}
